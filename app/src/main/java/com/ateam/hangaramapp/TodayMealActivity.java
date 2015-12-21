aac75ae8ed75aeddabe0922067edb4106f7f554a
조석주 <chosj0704@gmail.com>
2015-12-21 16:51:53 +0900

    급식 캘린더의 내재된 치명적 버그 해결
    try catch 구문으로 해결

--- app/src/main/java/com/ateam/hangaramapp/TodayMealActivity.java
+++ app/src/main/java/com/ateam/hangaramapp/TodayMealActivity.java
@@ -3,6 +3,7 @@ package com.ateam.hangaramapp;
 import android.app.ProgressDialog;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import java.util.Calendar;
+
 import com.squareup.timessquare.CalendarPickerView;
 import java.util.Date;
 import android.widget.Toast;
@@ -25,22 +27,22 @@ public class TodayMealActivity extends AppCompatActivity {
         @Override
         public void callbackMethod(ParseSen a) {
 
-            DBHelper helper = new DBHelper(TodayMealActivity.this, DBHelper.DB_FILE_NAME, null,1);
+            DBHelper helper = new DBHelper(TodayMealActivity.this, DBHelper.DB_FILE_NAME, null, 1);
             SQLiteDatabase db = helper.getReadableDatabase();
             String str = "";
-            int dbSize=0, dbLastday=-1;
+            int dbSize = 0, dbLastday = -1;
             boolean dbCheck[] = new boolean[33];
-            for(int i=0;i<32;i++){
+            for (int i = 0; i < 32; i++) {
                 dbCheck[i] = false;
             }
             int parseLastday;
 
-            Log.i("info", "제일 최근 정보 : "+a.getLastday()+"일자 정보");
+            Log.i("info", "제일 최근 정보 : " + a.getLastday() + "일자 정보");
 
-            Cursor cursor = db.rawQuery("select * from "+DBHelper.TODAYMEAL_TABLE_NAME, null);
+            Cursor cursor = db.rawQuery("select * from " + DBHelper.TODAYMEAL_TABLE_NAME, null);
 
             int date;
-            while(cursor.moveToNext()) {
+            while (cursor.moveToNext()) {
                 date = cursor.getInt(1);
                 str += cursor.getInt(0) //id값; 신경 안써도 됨
                         + " : date "
@@ -50,40 +52,39 @@ public class TodayMealActivity extends AppCompatActivity {
                         + ", dinner = "
                         + cursor.getString(3)  //저녁
                         + "\n";
-                if(cursor.getInt(1) > dbLastday) {
+                if (cursor.getInt(1) > dbLastday) {
                     dbLastday = date;
                 }
                 dbSize++;
-                if(date/10000==a.getYear() && (date%10000)/100==a.getMonth())
-                    dbCheck[(cursor.getInt(1))%100]=true;
+                if (date / 10000 == a.getYear() && (date % 10000) / 100 == a.getMonth())
+                    dbCheck[(cursor.getInt(1)) % 100] = true;
             }
 
-            if(a.getLastday() == -1){
+            if (a.getLastday() == -1) {
                 // 인터넷 연결이 영 이상하단 말이야
                 Log.i("info", "인터넷 확인좀...?");
                 progDialog.dismiss();
                 return;
             }
 
-            parseLastday = a.getYear()*10000+a.getMonth()*100+a.getLastday();
+            parseLastday = a.getYear() * 10000 + a.getMonth() * 100 + a.getLastday();
 
             // 새로운 정보가 또 있네?
             Log.i("info", "dbLastday = " + dbLastday + " // parseLastday = " + parseLastday);
 
 
-            if(dbLastday != parseLastday) {
+            if (dbLastday != parseLastday) {
                 for (int i = 1; i <= 31; i++) {
                     if (a.isMenuExist(i)) { // 파싱했는데 해당
                         if (!dbCheck[i]) { // 이미 있엉
 
-                            date = a.getYear()*10000 + a.getMonth() *100 + i;
-                            Log.i("info","insert into " + DBHelper.TODAYMEAL_TABLE_NAME + " (date, lunch, dinner) values (" + date + ", '" + a.getLunch(i) + "', '" + a.getDinner(i) + "');");
+                            date = a.getYear() * 10000 + a.getMonth() * 100 + i;
+                            Log.i("info", "insert into " + DBHelper.TODAYMEAL_TABLE_NAME + " (date, lunch, dinner) values (" + date + ", '" + a.getLunch(i) + "', '" + a.getDinner(i) + "');");
                             helper.insert("insert into " + DBHelper.TODAYMEAL_TABLE_NAME + " (date, lunch, dinner) values (" + date + ", '" + a.getLunch(i) + "', '" + a.getDinner(i) + "');");
                         }
                     }
                 }
-            }
-            else {
+            } else {
                 Log.i("info", "이미 정보가 있다!");
                 Log.i("info", "TODAYMEAL_DBLOG: " + str);
             }
@@ -115,24 +116,28 @@ public class TodayMealActivity extends AppCompatActivity {
 
         int[] dateBundle = getDateRange();
 
-        int startYear = dateBundle[0]/10000;
-        int startMonth = ((dateBundle[0]%10000)/100);
-        int startDay = dateBundle[0]%100;
+        //
+        int startYear = dateBundle[0] / 10000;
+        int startMonth = ((dateBundle[0] % 10000) / 100);
+        int startDay = dateBundle[0] % 100;
         Calendar calStart = Calendar.getInstance();
-        calStart.set(startYear, startMonth-1, startDay);
+        calStart.set(startYear, --startMonth, startDay);
         Log.i("info", "startYear = " + startYear + " 입니다.");
 
-        int endYear = dateBundle[1]/10000;
-        int endMonth = ((dateBundle[0]%10000)/100);
-        int endDay = dateBundle[1]%100;
+        int endYear = dateBundle[1] / 10000;
+        int endMonth = ((dateBundle[0] % 10000) / 100);
+        int endDay = dateBundle[1] % 100;
         Calendar calEnd = Calendar.getInstance();
-        calEnd.set(endYear, endMonth-1, endDay+1);
+        calEnd.set(endYear, --endMonth, ++endDay);
 
         //급식 선택 달력에 표시되는 날짜의 범위를 설정한다.
         CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
         Date today = new Date();
-        calendar.init(calStart.getTime(), calEnd.getTime())
-                .withSelectedDate(today);
+        calendar.init(calStart.getTime(), calEnd.getTime());
+        try {
+            calendar.selectDate(today);
+        }catch (IllegalArgumentException exception){
+        }
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -152,34 +157,31 @@ public class TodayMealActivity extends AppCompatActivity {
         });
     }
 
-    public int[] getDateRange(){
+    public int[] getDateRange() {
 
-        DBHelper helper = new DBHelper(TodayMealActivity.this, DBHelper.DB_FILE_NAME, null,1);
+        DBHelper helper = new DBHelper(TodayMealActivity.this, DBHelper.DB_FILE_NAME, null, 1);
         SQLiteDatabase db = helper.getReadableDatabase();
         Cursor cursor = db.rawQuery("select * from " + DBHelper.TODAYMEAL_TABLE_NAME, null);
 
         int focusedDate;
-       while(cursor.moveToNext()) {
+        while (cursor.moveToNext()) {
             focusedDate = cursor.getInt(1);
-            if(startDate == 0){
+            if (startDate == 0) {
                 startDate = focusedDate; //시작날짜에 포커스값 대입
-            }
-            else if(endDate == 0){
+            } else if (endDate == 0) {
                 endDate = focusedDate; //끝날짜에 포커스값 대입
-            }
-            else{
+            } else {
                 if (focusedDate < startDate) {
                     startDate = focusedDate; //시작날짜를 더 이전날짜가 존재하면 변경
                 }
-                if(focusedDate > endDate){
+                if (focusedDate > endDate) {
                     endDate = focusedDate; //끝날짜를 더 나중날짜가 존재하면 변경
                 }
             }
         }
-        Log.i("info", "startDate = "+startDate + " 입니다. \n endDate = "+endDate + " 입니다.");
+        Log.i("info", "startDate = " + startDate + " 입니다. \n endDate = " + endDate + " 입니다.");
 
         int[] dateBundle = {startDate, endDate};
-        return(dateBundle);
+        return (dateBundle);
     }
-
 }
